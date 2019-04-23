8ae85cd6e2fa6a1d896700e4d8ba10c53452dd4e
carlos ramirez <carlos.ramirez1@unmsm.edu.pe>
2019-04-23 16:41:27 -0500

    first commit

---
+++ src/app/_nav.ts
@@ -0,0 +1,65 @@
+interface NavAttributes {
+  [propName: string]: any;
+}
+interface NavWrapper {
+  attributes: NavAttributes;
+  element: string;
+}
+interface NavBadge {
+  text: string;
+  variant: string;
+}
+interface NavLabel {
+  class?: string;
+  variant: string;
+}
+
+export interface NavData {
+  name?: string;
+  url?: string;
+  icon?: string;
+  badge?: NavBadge;
+  title?: boolean;
+  children?: NavData[];
+  variant?: string;
+  attributes?: NavAttributes;
+  divider?: boolean;
+  class?: string;
+  label?: NavLabel;
+  wrapper?: NavWrapper;
+}
+
+export const navItems: NavData[] = [
+  {
+    title: true,
+    name: 'Theme'
+  },
+  {
+    name: 'Colors',
+    url: '/theme/colors',
+    icon: 'icon-drop'
+  },
+  {
+    name: 'Typography',
+    url: '/theme/typography',
+    icon: 'icon-pencil'
+  },
+  {
+    title: true,
+    name: 'Components'
+  },
+  {
+    name: 'Charts',
+    url: '/charts',
+    icon: 'icon-pie-chart'
+  },
+  {
+    name: 'Widgets',
+    url: '/widgets',
+    icon: 'icon-calculator'
+  },
+  {
+    divider: true
+  },
+
+];
